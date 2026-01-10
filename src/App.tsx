af4c60bb8831b426caf0a32b62b514730fc2b824
MD Mostafa Niloy <mostafa.niloys@gmail.com>
2026-01-10 17:52:38 +0600

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -29,8 +29,8 @@ const Preloader = () => (
   >
     <div className="relative flex items-center justify-center">
       {/* Animated Rings */}
-      <div className="absolute w-32 h-32 border-4 border-blue-500/20 rounded-full animate-[spin_3s_linear_infinite]"></div>
-      <div className="absolute w-32 h-32 border-t-4 border-blue-500 rounded-full animate-[spin_1.5s_linear_infinite]"></div>
+      <div className="absolute w-32 h-32 border-4 border-blue-500/20 rounded-full animate-[spin_1s_linear_infinite]"></div>
+      <div className="absolute w-32 h-32 border-t-4 border-blue-500 rounded-full animate-[spin_1s_linear_infinite]"></div>
       
       {/* Logo Container */}
       <div className="relative w-24 h-24 bg-slate-900 rounded-full flex items-center justify-center shadow-lg border border-slate-800 z-10">
